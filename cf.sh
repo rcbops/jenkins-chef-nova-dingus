0310e9d066aa10a61f2d39da1243a87b6e6da9fc
Joseph W. Breu <breu@breu.org>
2012-10-05 08:39:40 -0500

    correctly install the package

--- cf.sh
+++ cf.sh
@@ -39,8 +39,8 @@ boot_cluster ${cluster[@]}
 wait_for_cluster_ssh ${cluster[@]}
 
 echo "Cluster booted... setting up vpn thing"
-x_with_cluster "installing brctl" ${cluster[@]} <<EOF
-install_package brctl
+x_with_cluster "installing bridge-utils" ${cluster[@]} <<EOF
+install_package bridge-utils
 EOF
 background_task "fc_do"
 setup_private_network eth0 br99 api ${cluster[@]}
