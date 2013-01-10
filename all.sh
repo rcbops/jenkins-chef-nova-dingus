b6d97c9380f361bb1301bed0725f82471c2191e8
galstrom21 <jshepher@rackspace.com>
2013-01-10 16:25:30 -0600

    forgot the override_attributes bit

--- all.sh
+++ all.sh
@@ -62,11 +62,11 @@ api_vrrp_ip="10.127.55.1${EXECUTOR_NUMBER}"
 db_vrrp_ip="10.127.55.10${EXECUTOR_NUMBER}"
 
 # add the lb service vips to the environment
-set_environment_attribute chef_server ${CHEF_ENV} "vips/nova-api" ${api_vrrp_ip}
-set_environment_attribute chef_server ${CHEF_ENV} "vips/nova-ec2-public" ${api_vrrp_ip}
-set_environment_attribute chef_server ${CHEF_ENV} "vips/keystone-service-api" ${api_vrrp_ip}
-set_environment_attribute chef_server ${CHEF_ENV} "vips/cinder-api" ${api_vrrp_ip}
-set_environment_attribute chef_server ${CHEF_ENV} "vips/swift-proxy" ${api_vrrp_ip}
+set_environment_attribute chef_server ${CHEF_ENV} "override_attributes/vips/nova-api" ${api_vrrp_ip}
+set_environment_attribute chef_server ${CHEF_ENV} "override_attributes/vips/nova-ec2-public" ${api_vrrp_ip}
+set_environment_attribute chef_server ${CHEF_ENV} "override_attributes/vips/keystone-service-api" ${api_vrrp_ip}
+set_environment_attribute chef_server ${CHEF_ENV} "override_attributes/vips/cinder-api" ${api_vrrp_ip}
+set_environment_attribute chef_server ${CHEF_ENV} "override_attributes/vips/swift-proxy" ${api_vrrp_ip}
 
 # Disable glance image_uploading
 set_environment_attribute chef-server ${CHEF_ENV} "override_attributes/glance/image_upload" "false"
