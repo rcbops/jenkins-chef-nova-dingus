ec6e6d9020c337f45991bae8d8dcddccc7c93896
galstrom21 <jshepher@rackspace.com>
2013-01-21 21:49:54 -0600

    Adding collect_tasks call

--- roush-multi-node.sh
+++ roush-multi-node.sh
@@ -18,6 +18,7 @@ x_with_server "installing roush-server" roush <<EOF
 curl -s "http://3199f7b8138ccd4a5141-1f05446a499ccbc56d624169ca685698.r0.cf1.rackcdn.com/install-server.sh" | bash
 EOF
 background_task "fc_do"
+collect_tasks
 
 # install roush-agent
 x_with_cluster "Installing Roush-Agent" node1 node2 node3 <<EOF
