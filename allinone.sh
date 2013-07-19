52cc18795e82444ae91d660c442a97d9a41fc09a
RCB Builder <rcb-deploy@lists.rackspace.com>
2013-07-19 09:53:40 -0500

    expand the list of the files grabbed as job artifacts

--- allinone.sh
+++ allinone.sh
@@ -5,7 +5,7 @@ START_TIME=$(date +%s)
 PACKAGE_COMPONENT=${PACKAGE_COMPONENT:-grizzly}
 
 GRAB_LOGFILES_ON_FAILURE=1
-JOB_ARCHIVE_FILES="/var/log,/etc"
+JOB_ARCHIVE_FILES="/var/log,/etc,/var/lib/nova/instances/*/*.xml,/var/lib/nova/instances/*/*.log}"
 
 source $(dirname $0)/chef-jenkins.sh
 
