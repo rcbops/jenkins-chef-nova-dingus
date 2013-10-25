e33be0bcf0c14ba3d520a9d5d7477a4bf592182b
invsblduck <invsblduck@gmail.com>
2013-10-25 15:13:27 -0700

    bugfix: only grep 'name' column for networks
    also, delete networks FIRST (which will delete resident subnets) and
    then see if any other subnets match (to be thorough i guess?).

--- cleanup-deployment.sh
+++ cleanup-deployment.sh
@@ -48,15 +48,12 @@ if [ $(stat $tmp_instances |grep -w Size |awk '{print $2}') = 1 \
 then
     echo
     echo -e "[-] ==> \e[1;41;33mNo instances match /$REGEX/\e[0m"
-    if grep -Eq "$REGEX" $tmp_networks $tmp_subnets; then
+    # just grep the name field
+    if awk '{print $4}' $tmp_networks $tmp_subnets |grep -Eq "$REGEX"; then
         echo
-        echo "[*] But the following networks match:"
-        for f in $tmp_networks $tmp_subnets; do
-            head -3 $f
-            grep -Eh --color=always "$REGEX" $f
-            tail -1 $f
-            echo
-        done
+        echo "[*] But the following networks or subnets match:"
+        awk '{print $4}' $tmp_networks $tmp_subnets |grep -Eq "$REGEX"
+        echo
         read -p "[*] Delete these networks? [y/N]: "
         if [ -z "$REPLY" ] || [[ ! "$REPLY" =~ ^[yY] ]]; then
             echo "[-] exiting."
@@ -69,6 +66,7 @@ then
     fi
 else
     # make sure we didn't grab too many instances
+    # (grep this file to skip table borders and headers)
     if [ $(grep -E "$REGEX" $tmp_instances |wc -l) -gt 5 ]
     then
         echo
@@ -85,6 +83,7 @@ else
     err=
     IFS_SAVE=$IFS
     IFS=$'\n'
+    # use grep to skip table borders and headers
     for line in $(grep -E "$REGEX" $tmp_instances); do
         id=$(echo $line |awk '{print $2}')
         name=$(echo $line |awk '{print $4}')
@@ -128,48 +127,19 @@ fi
 # delete networks
 # FIXME need generic functions for copy/pasted/tweaked code
 #
-if grep -Eq "$REGEX" $tmp_networks $tmp_subnets
-then
+if awk '{print $4}' $tmp_networks $tmp_subnets |grep -Eq "$REGEX"; then
     # make sure we don't delete too many networks by accident
-    if [ $(grep -E "$REGEX" $tmp_networks $tmp_subnets |wc -l) -gt 4 ]
+    if [ $(awk '{print $4}' $tmp_networks |grep -E "$REGEX" |wc -l) -gt 2 ]
     then
         echo
-        echo "[*] Regex /$REGEX/ matches > 4 networks:"
-        for f in $tmp_networks $tmp_subnets; do
-            head -3 $f
-            grep -Eh --color=always "$REGEX" $f
-            tail -1 $f
-            echo
-        done
+        echo "[*] Regex /$REGEX/ matches > 2 networks:"
+        awk '{print $4}' $tmp_networks |grep -E "$REGEX"
+        echo
         read -p "[*] Really delete networks? [y/N]: "
         if [ -z "$REPLY" ] || [[ ! "$REPLY" =~ ^[yY] ]]; then
             echo "[-] exiting."
             exit 2
         fi
-    fi
-
-    err=
-    IFS_SAVE=$IFS
-    IFS=$'\n'
-    for line in $(grep -E "$REGEX" $tmp_subnets); do
-        id=$(echo $line |awk '{print $2}')
-        name=$(echo $line |awk '{print $4}')
-        if [ -z "$id" -o -z "$name" ]; then
-            echo "[?] skipping bad data:"
-            echo "[?] $line"
-            continue
-        fi
-        echo "[+] Deleting subnet $name ($id)"
-        set +e; quantum subnet-delete $id >/dev/null || err=true; set -e
-    done
-    IFS=$IFS_SAVE
-
-    if [ -n "$err" ]; then
-        echo
-        echo "[-] Failed to delete one or more subnets :("
-        echo "[-] ==> Not deleting quantum networks!"
-        echo "[-] ==> exiting."
-        exit 1
     fi
 
     err=
@@ -194,8 +164,47 @@ then
         echo "[-] exiting."
         exit 1
     fi
+
+    # refresh list of subnets and see if anything still matches
+    quantum subnet-list >$tmp_subnets
+    if awk '{print $4}' $tmp_subnets |grep -Eq "$REGEX"
+    then
+        echo
+        echo "[*] There are still matching subnets!:"
+        awk '{print $4}' $tmp_subnets |grep -E "$REGEX"
+        echo
+        read -p "[*] Clean these up too? [y/N]: "
+        if [ -z "$REPLY" ] || [[ ! "$REPLY" =~ ^[yY] ]]; then
+            echo "[-] exiting."
+            exit 0
+        fi
+    fi
+
+    err=
+    IFS_SAVE=$IFS
+    IFS=$'\n'
+    for line in $(grep -E "$REGEX" $tmp_subnets); do
+        id=$(echo $line |awk '{print $2}')
+        name=$(echo $line |awk '{print $4}')
+        if [ -z "$id" -o -z "$name" ]; then
+            echo "[?] skipping bad data:"
+            echo "[?] $line"
+            continue
+        fi
+        echo "[+] Deleting subnet $name ($id)"
+        set +e; quantum subnet-delete $id >/dev/null || err=true; set -e
+    done
+    IFS=$IFS_SAVE
+
+    if [ -n "$err" ]; then
+        echo
+        echo "[-] Failed to delete one or more subnets"
+        echo "[-] exiting."
+        exit 1
+    fi
+
 fi
 
 echo
-echo "[*] clean ;)"
+echo "[*] done"
 echo
